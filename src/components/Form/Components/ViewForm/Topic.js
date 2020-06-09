2bb62fff87b2dfe4ce171896530a1f2b48195ad1
Luiz Francisco <luizfrancisco2000@gmail.com>
2020-06-09 13:11:21 -0400

    Nova tela

--- src/components/Form/Components/ViewForm/Topic.js
+++ src/components/Form/Components/ViewForm/Topic.js
@@ -1,18 +1,18 @@
-import React,{Component} from 'react'
-import {elementsView } from './Elements'
+import React, { Component } from 'react'
+import { elementsView } from './Elements'
 
-const Topic = props =>{
+const Topic = props => {
     var questions = [];
 
     questions = props.questions;
     return (
-    <div className="topicView">
-        <h3>{props.title}
-        </h3>
-        {questions.map(element => <div style={{marginTop:15}}> {element.tipoResposta} {React.createElement(elementsView[element.tipoResposta], element)} </div>)}
+            <div className="TopicView">
+                <h3>{props.title}
+                </h3>
+                {questions.map(element => <div style={{ marginTop: 15 }}>{React.createElement(elementsView[element.tipoResposta], element)} </div>)}
 
-    </div>
+            </div>
     )
 }
 
-export {Topic}
+export { Topic }
